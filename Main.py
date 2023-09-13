26baf32dacf0d8595e1bd34b03221b50beb6cca6
Christian Villegas <99997132+Chrivill02@users.noreply.github.com>
2023-09-12 19:15:44 -0600

    Cree archivos py
    Lo mismo

---
+++ Main.py
@@ -0,0 +1,30 @@
+#Métodos para cultivos
+
+class Cultivo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Tomate(Cultivo):
+    def __init__(self, nombre, color):
+        super().__init__(nombre)
+        self.color = color
+
+
+#Métodos de animales
+
+
+
+
+
+
+
+#Espacio del tiempo
+
+
+
+
+
+
+#Espacio de comercio
+
+
